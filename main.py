7bd5b75886ad4923953329fa9ced8998ca9d071e
nikcassini <buranskydo@gmail.com>
2023-06-08 09:41:04 +0200

    print

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("TEST")
+import wordList
+
+# Základní proměnné
+win = False
+wordGuess = wordList.getWord()
+wordRevealed = wordGuess
+
+print(wordGuess)
+
+# Zašifrovat slovo, aby nebyly vidět písmenka
